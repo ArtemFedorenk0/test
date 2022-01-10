e5e4593555e732cd9ddd9a83ba660cccacdb712d
Федоренко Артем <fedorenko.a.wezom@gmail.com>
2022-01-10 14:23:17 +0200

    lessons

--- app/Repositories/BlogPostRepository.php
+++ app/Repositories/BlogPostRepository.php
@@ -18,4 +18,26 @@ class BlogPostRepository extends CoreRepository
     {
         return Model::class;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getAllWithPaginate()
+    {
+        $columns = [
+          'id', 'title', 'slug', 'is_published', 'published_at', 'user_id', 'category_id',
+        ];
+
+        $result = $this->startConditions()
+            ->select($columns)
+            ->orderBy('id', 'DESC')
+            ->with([
+                'category' => function ($query) {
+                    $query->select(['id', 'title']);
+                },
+                'user:id,name'])
+            ->paginate(25);
+
+        return $result;
+    }
 }
